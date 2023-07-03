851c8b58498ecbc0ab91cc73cfd2734330b23f21
macshop <tml.murod@mail.ru>
2023-07-03 18:53:12 +0500

    four lesson

---
+++ lib/models/member_model.dart
@@ -0,0 +1,18 @@
+class Member{
+  String uid = "";
+  String fullname = "";
+  String email = "";
+
+  Member(this.fullname, this.email);
+
+  Member.frromJson(Map<String, dynamic> json)
+    : uid = json['uid'],
+      fullname = json['fullname'],
+      email = json['email'];
+
+  Map<String, dynamic> toJson() => {
+    'uid': uid,
+    'fullname': fullname,
+    'email': email
+  };
+}
